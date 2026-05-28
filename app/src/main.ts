147eae718a432eb70df7cc08d777939c244357c1
guojc <guo.jianchao@outlook.com>
2026-05-28 10:54:49 +0800

    禁用鼠标右键菜单

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,11 @@ import router from './router'
 import App from './App.vue'
 import './styles/base.css'
 
+// 禁用鼠标右键菜单，提升原生桌面软件体验
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault()
+})
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
